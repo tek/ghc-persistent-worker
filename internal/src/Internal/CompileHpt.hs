66ef29f4495614ece7239265e158d4c5c22d6dab
Torsten Schmits <git@tryp.io>
2025-08-28 19:39:52 +0200

    Skip recompilation check in `compileOne` by setting `-fforce-recomp`
    Change-Id: Ic47f009e326e92be739a9fe8a1b65041b9b7582c

--- internal/src/Internal/CompileHpt.hs
+++ internal/src/Internal/CompileHpt.hs
@@ -16,6 +16,7 @@ import GHC (
   mgLookupModule,
   )
 import GHC.Driver.Env (HscEnv (..), hscActiveUnitId, hscUpdateHUG)
+import GHC.Driver.DynFlags (gopt_set)
 import GHC.Driver.Errors.Types (GhcMessage (..))
 import GHC.Driver.Make (summariseFile)
 import GHC.Driver.Monad (modifySession)
@@ -103,8 +104,12 @@ compileModuleWithDepsInHpt logVar target = do
       ppr summary.ms_location $$
       "summary home unit:" <+> ppr summary.ms_hspp_opts.homeUnitId_ $$
       "env home unit:" <+> ppr (hscActiveUnitId hsc_env)
-    result <- compileOne hsc_env summary 1 100000 Nothing (HomeModLinkable Nothing Nothing)
+    result <- compileOne hsc_env (forceRecomp summary) 1 100000 Nothing (HomeModLinkable Nothing Nothing)
     cleanCurrentModuleTempFilesMaybe (hsc_logger hsc_env) (hsc_tmpfs hsc_env) summary.ms_hspp_opts
     pure result
   modifySession (addDepsToHscEnv [hmi])
   pure (Just ModuleArtifacts {iface, bytecode = homeMod_bytecode hm_linkable})
+  where
+    -- This bypasses another recompilation check in 'compileOne'
+    forceRecomp summary =
+      summary {ms_hspp_opts = gopt_set summary.ms_hspp_opts Opt_ForceRecomp}
